e8911d6cec6c6d497a0c9b15006aa59cfee7257d
Khang Dui <khangduyb4@gmail.com>
2025-08-27 22:52:18 +0700

    Initialize services

---
+++ src/EscapeRoom/Assets/Scripts/Base/DI/ServiceInitializer.cs
@@ -0,0 +1,12 @@
+/// <summary>
+/// Default service initializer. It is example.
+/// </summary>
+public class ServiceInitializer : IServiceInitializer
+{
+    /// <inheritdoc/>
+    public void InitializeServices(ServiceContainer serviceContainer)
+    {
+        serviceContainer.Register<ILoadSceneService>(new LoadSceneService());
+        serviceContainer.Register<IEventBusService>(new EventBusService());
+    }
+}
