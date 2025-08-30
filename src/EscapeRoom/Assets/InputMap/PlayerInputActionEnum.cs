bf754a1cba571d284a6475e88de98d60514570d6
Khang Dui <khangduyb4@gmail.com>
2025-08-30 09:52:51 +0700

    Create player input map

---
+++ src/EscapeRoom/Assets/InputMap/PlayerInputActionEnum.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// InputActionEnum is an enumeration that defines the different input actions for the player.
+/// </summary>
+public enum PlayerInputActionEnum
+{
+    Move = 0,
+    Jump = 1,
+    Sprint = 2,
+}
