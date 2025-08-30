5d98ae2f40ff11f3691c2f3616c7eb1038c8753d
Khang Dui <khangduyb4@gmail.com>
2025-08-30 09:56:27 +0700

    Set up animator and adjust animation, Import new input system

---
+++ src/EscapeRoom/Assets/Scripts/Components/PlayerAnimationEnum.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// Enum for player animation states
+/// This enum is used to define the different animation states for the player character.
+/// </summary>
+public enum PlayerAnimationEnum
+{
+    Idle = 0,
+    Jump = 1,
+}
